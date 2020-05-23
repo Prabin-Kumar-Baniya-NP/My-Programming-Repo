115ea9f3baaca651482b1adad46aae475071633d
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2020-05-23 18:39:48 +0545

    Committed in Linux

--- Project-2-Flask-App-01/app.py
+++ Project-2-Flask-App-01/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import datetime
+import requests
+import json
 app = Flask(__name__)
 
 
@@ -13,3 +15,17 @@ def contact_me():
     name="Prabin Kumar Baniya"
     email="pra44bin@gmail.com"
     return render_template('contact.html', name=name, email=email)
+@app.route('/more')
+def more():
+    message="If you like our service please contact us"
+    return render_template()
+
+@app.route('/weather')
+def getweather():
+    url = "http://api.openweathermap.org/data/2.5/weather?"
+    api_key = "0c6d92daecea782cc14db6a6d92e4e8b"
+    location="Butwal"
+    complete_url= url + "q=" + location + "&APPID=" + api_key
+    response=requests.get(complete_url)
+    x=response.json()
+    return x
